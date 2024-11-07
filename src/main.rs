31b8edbc6e3006313210cfa9a9ca3a1cdb520bde
Mairbek Khadikov <mairbek@conduit.xyz>
2024-11-06 20:48:55 -0800

    challenge 2

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 enum Cli {
     Chalenge1 {
-        #[structopt(short, long)]
+        #[structopt(long)]
         input: String,
+    },
+    Challenge2 {
+        #[structopt(long)]
+        input1: String,
+        #[structopt(long)]
+        input2: String,
     },
 }
 
@@ -20,12 +26,30 @@ fn decode_hex(hex: &str) -> Vec<u8> {
     bytes
 }
 
+fn encode_hex(bytes: &[u8]) -> String {
+    let mut hex = String::new();
+    for byte in bytes {
+        hex.push_str(&format!("{:02x}", byte));
+    }
+    hex
+}
+
+fn xor_bytes(a: &[u8], b: &[u8]) -> Vec<u8> {
+    a.iter().zip(b.iter()).map(|(x, y)| x ^ y).collect()
+}
+
 fn main() {
     let args = Cli::from_args();
     match args {
         Cli::Chalenge1 { input } => {
             let bytes = decode_hex(&input);
             println!("{}", BASE64_STANDARD.encode(&bytes));
+        }
+        Cli::Challenge2 { input1, input2 } => {
+            let bytes1 = decode_hex(&input1);
+            let bytes2 = decode_hex(&input2);
+            let xor = xor_bytes(&bytes1, &bytes2);
+            println!("{}", encode_hex(&xor));
         }
     }
 }
